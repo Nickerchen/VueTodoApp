32f8b2aee4e5bfe14a8c85398371f63f04547e8d
James Friesen <James.Friesen@pixel.de>
2020-01-29 12:17:21 +0100

    todo toggle

---
+++ src/components/TodoList.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-container>
+    <v-layout text-center wrap>
+
+<div class="todo-list">
+      <v-list >
+        <v-list-item v-for="item in getTodos" :key="item.id">
+          <todo-list-item :item = "item"/>
+        </v-list-item>
+      </v-list>
+</div>
+
+    <div><v-switch v-model=displayAll></v-switch> Alle Anzeigen </div>
+     
+
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import TodoListItem from './TodoListItem';
+
+export default {
+  name: 'TodoList',
+
+  components: {
+    TodoListItem,
+  },
+
+  data: () => ({
+   displayAll: false,
+
+   todos: [
+   { id: 1, title: "Um 17 Uhr gehen", subtitle: "dringend", checked: true},
+   { id: 2, title: "Um 9 Uhr gehen", subtitle: "dringend", checked: false},
+   { id: 3, title: "Um 99 Uhr gehen", subtitle: "dringend", checked: false}
+    ]
+  }),
+
+  computed: {
+    getTodos() {
+      if ( this.displayAll) {
+        return this.todos;
+
+      } else {
+        return this.todos.filter( item => !item.checked)
+      }
+    }
+  }
+
+};
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="todo-list-item" >
+   <v-list-item  v-if="!displayAll || (displayAll && !todo.checked)"> 
+      <v-list-item-content> 
+            <v-icon color="green" v-if=item.checked >mdi-peace</v-icon>
+            <v-icon color="red" v-if=!item.checked >mdi-nuke</v-icon>
+            <v-list-item-title> {{item.title}}</v-list-item-title>
+            <v-list-item-subtitle>{{item.subtitle}}</v-list-item-subtitle>
+            <v-checkbox v-model=item.checked></v-checkbox> 
+          </v-list-item-content>
+      </v-list-item>
+</div>
+</template>
+
+<script>
+export default {
+data: () => ({
+
+}),
+
+    props:[
+        'item',
+        'displayAll'
+    ]
+
+}
+</script>
+
+<style>
+
+</style>
